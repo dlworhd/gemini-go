c51eda9aeb16fb04b3c357732316c5253324c074
dlworhd <dlwodud821@gmail.com>
2025-08-09 23:13:19 +0900

    refactor: 코드 및 주석 수정

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// GEMINI_BASE_URL=https://generativelanguage.googleapis.com/v1beta
+// GEMINI_API_KEY=your-api-key
+// GEMINI_MODEL=gemini-2.0-flash
+
 func main() {
 	godotenv.Load()
 	gemini := model.Gemini{}
 
-	prompt := "Hello World!"
+	prompt := "your prompt"
 	gemini.Generate(prompt)
 }
